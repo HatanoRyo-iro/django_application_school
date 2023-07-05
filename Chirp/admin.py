4d52700e290eef22028c6570079282afb3b69724
RyoHatano <ryohatano0809@gmail.com>
2023-07-05 14:03:01 +0900

    Add the created table to admin

--- Chirp/admin.py
+++ Chirp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Post, Group, Friend, Good
+
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Group)
+admin.site.register(Friend)
+admin.site.register(Good)
