aa15257c2872541d5e9fe0c17cd416f5c90437b8
RyoHatano <ryohatano0809@gmail.com>
2023-07-08 14:02:00 +0900

    create additional form

--- Chirp/forms.py
+++ Chirp/forms.py
@@ -4,6 +4,36 @@ from django.contrib.auth.models import User
 from .models import Post, Group, Friend, Good
 
 
+# グループチェックボックスフォーム
+class GroupCheckboxForm(forms.Form):
+    def __init__(self, user, *args, **kwargs):
+        super(GroupCheckboxForm, self).__init__(*args, **kwargs)
+        public = User.objects.get(username='sample').first()
+        self.fields['groups'] = forms.MultipleChoiceField(choices=[(item.group_name, item.group_name) for item in Group.objects.filter(owner__in=[user, public])],
+                                                          widget = forms.CheckboxSelectMultiple(attrs={'class' : 'form-check-input'}))
+
+
+# グループ選択メニューフォーム
+class GroupSelectMenuForm(forms.Form):
+    def __init__(self, user, *args, **kwargs):
+        super(GroupSelectMenuForm, self).__init__(*args, **kwargs)
+        self.fields['groups'] = forms.ChoiceField(choices=[('-', '-')] + [(item.group_name, item.group_name) for item in Group.objects.filter(owner=user)],
+                                                  widget=forms.Select(attrs={'class' : 'form-control'}))
+
+
+# フレンドのチェックボックスフォーム
+class FriendsCheckboxForm(forms.Form):
+    def __init__(self, user, friends=[], vals=[], *args, **kwargs):
+        super(FriendsCheckboxForm, self).__init__(*args, **kwargs)
+        self.friends['friends'] = forms.MultipleChoiceField(choices=[(item.user_id, item.user_id) for item in friends],
+                                                            widget=forms.CheckboxSelectMultiple(attrs={'class' : 'form-check-input'}), initial=vals)
+    
+    
+# グループ作成フォーム
+class CreateGroupForm(forms.ModelForm):
+    group_name = forms.ChoiceField(label='グループ名', max_length=50, 
+                                   widget=forms.TextInput(attrs={'class' : 'form-control'}))
+
 # 投稿フォーム
 class PostForm(forms.ModelForm):
     content = forms.CharField(label='投稿', max_length=280, widget=forms.Textarea(attrs={'class' : 'form-control', 'rows' : 2}))
