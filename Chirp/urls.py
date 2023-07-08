4c85ec105e39c3307d71ee84446479fa8fd70475
RyoHatano <ryohatano0809@gmail.com>
2023-07-08 14:07:25 +0900

    edit urlpatterns

--- Chirp/urls.py
+++ Chirp/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("signup/", views.SignupView.as_view(), name="signup"),
     path('', views.home, name='home'),
+    path('<int:page>/', views.home, name='home'),
+    path('groups/', views.groups, name='groups'),
+    path('add/',views.add, name='add'),
+    path('create_group/', views.create_group, name='create_group'),
+    path('post', views.post, name='post'),
+    path('share/<int:shared_id>/', views.share, name='share'),
+    path('good/<int:good_id>/', views.good, name='good'),
 ]
