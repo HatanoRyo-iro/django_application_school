a43fc96b52bd9e5c7efb65f1f82f85c4cead6351
RyoHatano <ryohatano0809@gmail.com>
2023-07-12 23:32:19 +0900

    mypost delete function

--- Chirp/urls.py
+++ Chirp/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('post', views.post, name='post'),
     path('share/<int:share_id>/', views.share, name='share'),
     path('good/<int:good_id>/', views.good, name='good'),
+    path('mypost_delete/<int:post_id>/', views.mypost_delete, name='mypost_delete'),
 ]
